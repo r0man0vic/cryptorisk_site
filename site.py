9e25077b4411b34e1622f60f1df126d4497efc78
mo <mo@mos-MacBook-Pro-2.local>
2026-02-19 18:06:17 -0500

    update site

--- site.py
+++ site.py
@@ -2,3 +2,15 @@ import streamlit as st
 #titles
 st.title("Crypto Risks")
 st.write("Project starting...")
+
+import streamlit as st
+import yfinance as yf
+
+st.set_page_config(page_title="Crypto Risks", layout="wide")
+
+st.title("Crypto Risks 📉")
+
+btc = yf.download("BTC-USD", period="6mo")
+
+st.line_chart(btc["Close"])
+st.write("Version 0.4")
